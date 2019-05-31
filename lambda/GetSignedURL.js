7079f9cf1decab38f5085ae64742e7f8df61a868
Sardo Ip <sardo@pixelactionstudio.com>
2019-05-31 11:12:53 +0800

    Create no cors and node v10 version

--- lambda/GetSignedURL.js
+++ lambda/GetSignedURL.js
@@ -2,27 +2,55 @@ var AWS = require('aws-sdk')
 var s3 = new AWS.S3();
 var bucketName = process.env.AWS_BUCKET_NAME
 
-exports.handler = (event, context) => {
-    if (!event.hasOwnProperty('contentType')) {
-        context.fail({ err: 'Missing contentType' })
-    }
-
-    if (!event.hasOwnProperty('filePath')) {
-        context.fail({ err: 'Missing filePath' })
-    }
-
-    var params = {
+exports.handler = async (event) => {
+    const params = {
         Bucket: bucketName,
         Key: event.filePath,
         Expires: 3600,
         ContentType: event.contentType
     }
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: {},
+    }
+    if (!event.hasOwnProperty('contentType')) {
+      response.body = JSON.stringify({
+        err: 'Missing contentType'
+      })
+    }
 
-    s3.getSignedUrl('putObject', params, (err, url) => {
-        if (err) {
-            context.fail({ err })
-        } else {
-            context.succeed({ url })
-        }
-    })
-}
+    if (!event.hasOwnProperty('filePath')) {
+      response.body = JSON.stringify({
+        err: 'Missing filePath'
+      })
+    }
+
+    try{
+      const url = await _getUrl(params);
+      response.body = JSON.stringify({
+        url
+      })
+    }catch(err){
+      response.body = JSON.stringify({
+        err: err
+      })
+    }
+    console.log(response);
+    return response;
+
+    function _getUrl(params){
+      return new Promise((resolve, reject) => {
+        s3.getSignedUrl('putObject', params, (err, url) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(url);
+            }
+        })
+      });
+    }
+};
